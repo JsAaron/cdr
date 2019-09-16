c8151e2c5d2520f7cbc2743a038341956bbd59af
jsaaron <401545553@qq.com>
2019-09-16 16:10:07 +0800

    123

--- ConsoleApp/ConsoleApp/Module1.vb
+++ ConsoleApp/ConsoleApp/Module1.vb
@@ -4,8 +4,9 @@ Imports System.Text
 
 Module Module1
 
+
   Public Function log(name, value)
-    Console.WriteLine("{""" & name & """:""" & value & """}")
+    Console.WriteLine("{""" + name + """:""" + value + """}")
     Return False
   End Function
 
@@ -53,41 +54,8 @@ Module Module1
 
   End Function
 
-  Sub createDocumnet()
-    Dim pia_type As Type = Type.GetTypeFromProgID("CorelDRAW.Application")
-    Dim app As Application = Activator.CreateInstance(pia_type)
 
-    app.Visible = True
-
-    '启动休眠
-    Threading.Thread.Sleep(3000)
-
-    Dim b() = Split(Command, " ")
-    Dim path As String
-    Dim createJson As Boolean = False
-    If b.Count = 1 Then
-      path = b(0)
-    End If
-    If b.Count = 2 Then
-      path = b(0)
-      If b(1) = "fontJson:true" Then
-        createJson = True
-      End If
-    End If
-
-    '如果有命令路径参数，打开对应的cdr
-    If path <> "" Then
-      app.OpenDocument(path)
-    End If
-
-    Dim doc As Document = app.ActiveDocument
-
-    '如果没有文档
-    If app.Documents.Count = 0 Then
-      log("error", "0002")
-      Exit Sub
-    End If
-
+  Sub outputResult(app, doc, createJson)
 
     '指定毫米
     doc.Unit = 3
@@ -95,54 +63,112 @@ Module Module1
     Dim height = app.ActivePage.SizeHeight
     Dim tW = """width"""
     Dim tH = """height"""
-    Dim data = "{" & tW & ":""" & width & """," & tH & ":""" & height & """}"
+        Dim data = "{" & tW & ":""" & width & """," & tH & ":""" & height & """}"
 
-    '创建当前文字的json
-    If createJson = True Then
+        log("pagesize", data)
 
-      '获取当前的应用的字体
-      Dim names = getFontNames(doc)
-      Dim i As Integer
-      Dim fontList As New ArrayList()
-      Dim str As String = ""
+        '创建当前文字的json
+        If createJson = True Then
 
-      For i = 0 To names.Count - 1
-        Dim IsExist As Boolean = True
-        For j As Integer = 0 To fontList.Count - 1
-          If fontList(j).ToString() = names(i) Then
-            IsExist = False
-            Exit For
-          End If
-        Next
-        If IsExist Then
-          fontList.Add(names(i))
-          Dim empty As String = ""
-          str = str + "{" + """fontname""" + ":""" + empty + """," + """familyname""" + ":""" + names(i) + """," + """postscriptname""" + ":""" + empty + """},"
+            'log("log", "开始搜索文档字体")
+            '获取当前的应用的字体
+            Dim names = getFontNames(doc)
+            Dim i As Integer
+            Dim fontList As New ArrayList()
+            Dim str As String = ""
+
+            For i = 0 To names.Count - 1
+                Dim IsExist As Boolean = True
+                For j As Integer = 0 To fontList.Count - 1
+                    If fontList(j).ToString() = names(i) Then
+                        IsExist = False
+                        Exit For
+                    End If
+                Next
+                If IsExist Then
+                    fontList.Add(names(i))
+                    Dim empty As String = ""
+                    str = str + "{" + """fontname""" + ":""" + empty + """," + """familyname""" + ":""" + names(i) + """," + """postscriptname""" + ":""" + empty + """},"
+                End If
+            Next
+
+            '去掉最后一个，
+            str = Left(str, Len(str) - 1)
+            str = "[" + str + "]"
+            Dim p() = Split(doc.FileName, ".")
+            Dim fs As FileStream = File.Create(doc.FilePath + p(0) + ".json")
+            Dim info As Byte() = New UTF8Encoding(True).GetBytes(str)
+            fs.Write(info, 0, info.Length)
+            fs.Close()
+            log("fontjson", "已生成")
+
         End If
-      Next
 
-      '去掉最后一个，
-      str = Left(str, Len(str) - 1)
-      str = "[" + str + "]"
-      Dim fs As FileStream = File.Create(doc.FilePath + "font.json")
-      Dim info As Byte() = New UTF8Encoding(True).GetBytes(str)
-      fs.Write(info, 0, info.Length)
-      fs.Close()
-      '文件写入完成
-      log("finish", "0001")
-    End If
+    End Sub
 
-    '输出参数
-    Console.Write(data)
 
-  End Sub
+    Dim lineCount = 3
 
+    Sub checkLine()
+
+        Dim path As String
+        Dim createJson As Boolean = False
+        Dim b() = Split(Command, " ")
+        If b.Count = 1 Then
+            path = b(0)
+        End If
+        If b.Count = 2 Then
+            path = b(0)
+            If b(1) = "fontJson:true" Then
+                createJson = True
+            End If
+        End If
+
+        Dim pia_type As Type = Type.GetTypeFromProgID("CorelDRAW.Application")
+        Dim app As Application = Activator.CreateInstance(pia_type)
+        app.Visible = True
+
+        Try
+            '如果有命令路径参数，打开对应的cdr
+            If path <> "" Then
+                app.OpenDocument(path)
+                'log("log", "打开CRD文件")
+            End If
+
+            Dim doc As Document = app.ActiveDocument
+
+            '如果没有文档
+            If app.Documents.Count = 0 Then
+                log("error", "没有找到活动文档")
+                Exit Sub
+            End If
+
+            'log("log", "打开活动文档")
+
+            outputResult(app, doc, createJson)
+
+            ' MsgBox(11)
+
+        Catch ex As Exception
+            log("error", "CorelDRAW执行失败，休眠3秒后开始下一次执行")
+
+            If lineCount = 0 Then
+                log("error", "CorelDRAW执行失败")
+                Exit Sub
+            End If
+            lineCount = lineCount - 1
+            Threading.Thread.Sleep(3000)
+            checkLine()
+
+        End Try
+
+    End Sub
 
   Sub Main()
     Try
-      createDocumnet()
+      checkLine()
     Catch ex As Exception
-      log("error", "0001")
+      log("error", "CorelDRAW异常,无法启动")
     End Try
   End Sub
 
--- call/command/app.py
+++ call/command/app.py
@@ -1,11 +1,12 @@
 import subprocess
 import sys
 
+# obj = subprocess.Popen(cmdStr, stdin=subprocess.PIPE, stdout=subprocess.PIPE ,stderr=subprocess.PIPE)
+# print(obj.stdin.write('ls\n'.encode('utf-8')))
 
 cmdStr = ["D:\\github\\cdr\\ConsoleApp\\ConsoleApp\\bin\\Debug\\ConsoleApp.exe",
-          "C:\\Users\\Administrator\\Desktop\\cdr\\19规范版横版.cdr", "fontJson:true"]
-pi = subprocess.Popen(cmdStr, shell=True, stdout=subprocess.PIPE)
-
-
-lines = pi.stdout.readlines()
-print(lines)
+          "C:\\Users\\Administrator\\Desktop\\cdr\\19规范版横版.cdr","fontJson:true"]
+child = subprocess.Popen(cmdStr, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+for line in child.stdout.readlines():
+    output = line.decode('GBK')
+    print(output)
