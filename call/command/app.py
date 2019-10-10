40cd8ceee5e9368664684eb44b0fa26d01912dd5
jsaaron <401545553@qq.com>
2019-10-10 16:36:42 +0800

    udpate

--- call/command/app.py
+++ call/command/app.py
@@ -6,23 +6,26 @@ import json
 # print(obj.stdin.write('ls\n'.encode('utf-8')))
 
 # 路径
-path ='C:\\Users\\Administrator\\Desktop\\黄蓝-黑.cdr'
-# 配置 
+path = 'C:\\Users\\Administrator\\Desktop\\黄蓝-黑.cdr'
+# 配置
 # open 打开文档 参数 (open,path)
 # get:pageSize 获取页面尺寸 (get:pageSize,path)
 # get:fontJson 获取字体json (get:fontJson,path)
-# get:text 提取文本数据（名片）(get:text,path)  
-# set:text 提取文本数据（名片）(set:text,externalData,path)  
-command = "set:extract"
-# 外部数据
-externalData = "{'job':'老大','name':'沉稳'}"
+# get:text 提取文本数据（名片）(get:text,path)
+# set:text 提取文本数据（名片）(set:text,externalData,path)
+# set:style 设置样式文件 (set:style,样式文件路径, 文档路径)
+command = "set:text"
 
-cmdStr = [
-    "D:\\github\\cdr\\ConsoleApp\\ConsoleApp\\bin\\Debug\\ConsoleApp.exe", "get:text"]
+stylePath = "C:\\Users\\Administrator\\Desktop\\1618d6a4-e32c-11e9-b5e8-086266c80046.cdss"
+
+# 外部数据
+# externalData = "{'logo':'C:%5CUsers%5CAdministrator%5CDesktop%5Ctt.jpg'}"
+externalData = "{'address':'aaaaaaaaaaaa','url':''}"
+
+cmdStr = ["D:\\\github\\cdr\\ConsoleApp\\ConsoleApp\\bin\\Debug\\ConsoleApp.exe",command,externalData]
 
 child = subprocess.Popen(cmdStr, shell=True, stdout=subprocess.PIPE,
                          stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 for line in child.stdout.readlines():
     output = line.decode('UTF-8')
     print(output)
-
